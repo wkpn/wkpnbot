524d11bef9f8142efe56ad120fae2df7450c61c6
wkpn <yegornosov@gmail.com>
2021-03-16 23:52:39 +0300

    tweak update processing a bit

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ def build_application() -> Starlette:
     async def webhook_handler(request: Request) -> Response:
         raw_update = await request.json()
         telegram_update = Update(**raw_update)
-        await dp.process_update(telegram_update)
 
-        return Response(status_code=HTTP_200_OK)
+        try:
+            await dp.process_update(telegram_update)
+        finally:
+            return Response(status_code=HTTP_200_OK)
 
     routes = [
-        Route(f"/webhook-{secret}", webhook_handler, methods=["POST"]),
+        Route(f"/webhook-{secret}", webhook_handler, methods=["POST"])
     ]
 
     app = Starlette(routes=routes)
