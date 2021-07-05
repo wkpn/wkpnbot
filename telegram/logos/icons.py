e9f896abce679160f3d267b9291041bf2fb1f8c2
wkpn <yegornosov@gmail.com>
2021-07-05 16:43:36 +0300

    weird pm bug fix attempt

--- telegram/logos/icons.py
+++ telegram/logos/icons.py
@@ -5,5 +5,5 @@ from dataclasses import dataclass
 class Icons:
     GITHUB: str = "https://raw.githubusercontent.com/wkpn/wkpnbot/master/telegram/logos/github.png"
     LINKEDIN: str = "https://raw.githubusercontent.com/wkpn/wkpnbot/master/telegram/logos/linkedin.png"
-    PROTONMAIL: str = "https://raw.githubusercontent.com/wkpn/wkpnbot/master/telegram/logos/protonmail.png"
+    PROTONMAIL: str = "https://raw.githubusercontent.com/wkpn/wkpnbot/master/telegram/logos/pm.png"
     SIGNAL: str = "https://raw.githubusercontent.com/wkpn/wkpnbot/master/telegram/logos/signal.png"
