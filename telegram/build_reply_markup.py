bf4c7ef77e844ec40457823f1aeb28a46114d4fa
wkpn <yegornosov@gmail.com>
2021-07-07 03:10:21 +0300

    rmv unnecessary comma

--- telegram/build_reply_markup.py
+++ telegram/build_reply_markup.py
@@ -3,7 +3,7 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 def build_reply_markup(user_mode: bool) -> ReplyKeyboardMarkup:
     reply_keyboard_markup = ReplyKeyboardMarkup(
-        resize_keyboard=True, one_time_keyboard=False, row_width=1,
+        resize_keyboard=True, one_time_keyboard=False, row_width=1
     )
 
     if user_mode:
